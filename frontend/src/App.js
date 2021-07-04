1bea57bf567bb021d940fab0a98e0a0ae0e5f6e6
ashuacharya123 <71539355+ashuacharya123@users.noreply.github.com>
2021-07-04 10:17:46 +0545

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,17 @@ import "./App.css";
 import Homepage from "./Components/homepage/homepage";
 import Addpage from "./Components/Addpage/addpage";
 import Card from "./Components/card/card";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Homepage />
-      <Card />
-      <Addpage />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/" component={Card} />
+        <Route exact path="/addnote" component={Addpage} />
+      </div>
+    </Router>
   );
 }
 
