b066a6719da5bd5cbd25c7d7c79349c44f3b36c1
Anjana Poudel <anzana000@gmail.com>
2021-07-04 11:12:18 +0545

    card styling remaining

--- frontend/src/Components/card/card.js
+++ frontend/src/Components/card/card.js
@@ -1,31 +1,23 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React from 'react';
 import "./card.css";
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const Card = () => {
-  const [notes, setNotes] = useState([]);
-
-  useEffect(() => {
-    async function data() {
-      await axios
-        .get("/api/v1/note")
-        .then((res) => setNotes(res.data.data.notes))
-        .catch((err) => console.log(err));
-    }
-    data();
-  });
-  return (
-    <div className="cards">
-      {notes.map((note) => {
-        return (
-          <div className="card">
-            <li>{note.name}</li>
-            <li>{note.description}</li>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+    
+    return (
+        <div className = "card">
+            <div className="card-title">Title</div>
+            <div className="card-body">
+                <p>Last Modified date</p>
+                <div className="social-icons">
+                    <EditIcon />
+                    <DeleteIcon/>
+                </div>
+                    
+            </div>
+        </div>
+    )
+}
 
 export default Card;
