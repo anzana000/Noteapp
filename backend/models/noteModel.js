ede16902f786ddddd3d8db19e928ffa6001762e7
ashuacharya123 <71539355+ashuacharya123@users.noreply.github.com>
2021-07-04 19:27:08 +0545

    uff

--- backend/models/noteModel.js
+++ backend/models/noteModel.js
@@ -5,12 +5,14 @@ const noteSchema = new mongoose.Schema({
     type: String,
     required: [true, "A note must have a name"],
     trim: true,
+    maxlength: [10, "A note must have less or equal to 10 characters"],
   },
 
   description: {
     type: String,
     required: [true, "A note must have a description"],
     trim: true,
+    maxlength: [100, "A note must have less or equal to 100 characters"],
   },
   createdAt: {
     type: Date,
