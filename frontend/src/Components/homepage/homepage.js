c359ce8f309d766b0d04e5d4fe82b88e8c67a7b5
ashuacharya123 <71539355+ashuacharya123@users.noreply.github.com>
2021-07-04 10:20:42 +0545

    Update homepage.js

--- frontend/src/Components/homepage/homepage.js
+++ frontend/src/Components/homepage/homepage.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React from "react";
 import "./homepage.css";
-import AddIcon from '@material-ui/icons/Add';
+import AddIcon from "@material-ui/icons/Add";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
-    return (
-        <div className = "homepage">
-            <div className="homepage-title">
-                <h2>Add a new note</h2>
-                <span><AddIcon/></span>
-            </div>
+  return (
+    <div className="homepage">
+      <Link to="/addnote">
+        <div className="homepage-title">
+          <h2>Add a new note</h2>
+          <span>
+            <AddIcon />
+          </span>
         </div>
-    )
-}
+      </Link>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
