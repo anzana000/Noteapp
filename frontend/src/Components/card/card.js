64942a4fce95a27dd84ee973b4ed1efbdc964779
Anjana Poudel <anzana000@gmail.com>
2021-07-04 11:31:25 +0545

    card case unsolved

--- frontend/src/Components/card/card.js
+++ frontend/src/Components/card/card.js
@@ -1,23 +1,59 @@
-import React from 'react';
+// import React from 'react';
+// import "./card.css";
+// import EditIcon from '@material-ui/icons/Edit';
+// import DeleteIcon from '@material-ui/icons/Delete';
+
+// const Card = () => {
+    
+//     return (
+//         <div className="card-wrapper">
+//             <div className = "card">
+//             <div className="card-title">Title</div>
+//             <div className="card-body">
+//                 <p>Last Modified date</p>
+//                 <div className="social-icons">
+//                     <EditIcon />
+//                     <DeleteIcon/>
+//                 </div>
+                    
+//             </div>
+//         </div>
+//         </div>
+//     )
+// }
+
+// export default Card;
+
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import "./card.css";
-import EditIcon from '@material-ui/icons/Edit';
-import DeleteIcon from '@material-ui/icons/Delete';
 
 const Card = () => {
-    
-    return (
-        <div className = "card">
-            <div className="card-title">Title</div>
-            <div className="card-body">
-                <p>Last Modified date</p>
-                <div className="social-icons">
-                    <EditIcon />
-                    <DeleteIcon/>
-                </div>
-                    
-            </div>
-        </div>
-    )
-}
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    async function data() {
+      await axios
+        .get("/api/v1/note")
+        .then((res) => setNotes(res.data.data.notes))
+        .catch((err) => console.log(err));
+    }
+    data();
+  });
+  return (
+    <div className="cards">
+      {notes.map((note) => {
+        return (
+          <div className="card">
+                <ul>
+                     <li>{note.name}</li>
+            <li>{note.description}</li>
+           </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Card;
