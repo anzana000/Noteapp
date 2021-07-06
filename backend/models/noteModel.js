75efb9c360066d33ea541060030fbca05c6e40ab
Anjana Poudel <anzana000@gmail.com>
2021-07-06 12:24:07 +0545

    merge cancelled

--- backend/models/noteModel.js
+++ backend/models/noteModel.js
@@ -5,14 +5,14 @@ const noteSchema = new mongoose.Schema({
     type: String,
     required: [true, "A note must have a title"],
     trim: true,
-    maxlength:[10,'A name must be within 10 characters!']
+    maxlength: [10, "A name must be within 10 characters!"],
   },
 
   description: {
     type: String,
     required: [true, "A note must have a description"],
     trim: true,
-    maxlength:[200,"Description cannot be more than 200 characters"]
+    maxlength: [200, "Description cannot be more than 200 characters"],
   },
   createdAt: {
     type: Date,
