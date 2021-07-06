dc14970f69162f280b252d593f67f32e30084908
ashuacharya123 <71539355+ashuacharya123@users.noreply.github.com>
2021-07-06 09:31:48 +0545

    added delete function

--- frontend/src/Components/card/card.js
+++ frontend/src/Components/card/card.js
@@ -1,9 +1,7 @@
-
-
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import EditIcon from '@material-ui/icons/Edit';
-import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
 import "./card.css";
 
 const Card = () => {
@@ -12,32 +10,39 @@ const Card = () => {
   useEffect(() => {
     async function data() {
       await axios
-        .get("/api/v1/note")
+        .get("/api/v1/note?sort=-createdAt")
         .then((res) => setNotes(res.data.data.notes))
         .catch((err) => console.log(err));
     }
     data();
   });
-    
+
+  const deleteNote = (id) => {
+    async function del() {
+      await axios.delete(`/api/v1/note/${id}`);
+    }
+    del();
+  };
 
   return (
-    <div className="cards" >
+    <div className="cards">
       {notes.map((note) => {
-       
-        return (   
-         
-          <div className="card" key = {note._id}>
+        return (
+          <div className="card" key={note._id}>
             <div className="card-title">{note.name}</div>
             <div className="card-body">
               <p>{note.description}</p>
             </div>
-             <span className="socials">
-              <span className = "date">  {`Last modified: ${note.createdAt.split("T")[0]}`}</span>
-                <EditIcon />
+            <span className="socials">
+              <span className="date">
+                {`Last modified: ${note.createdAt.split("T")[0]}`}
+              </span>
+              <EditIcon />
+              <span onClick={() => deleteNote(note._id)}>
                 <DeleteIcon />
               </span>
-            </div>
-           
+            </span>
+          </div>
         );
       })}
     </div>
