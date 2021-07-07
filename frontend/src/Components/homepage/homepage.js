fc9af8deca7e880f3383bd40fb31c1712e8be086
Anjana Poudel <anzana000@gmail.com>
2021-07-07 10:32:48 +0545

    search ui added

--- frontend/src/Components/homepage/homepage.js
+++ frontend/src/Components/homepage/homepage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./homepage.css";
 import AddIcon from "@material-ui/icons/Add";
+import SearchIcon from '@material-ui/icons/Search';
 import { Link } from "react-router-dom";
 
 const Homepage = () => {
@@ -14,7 +15,12 @@ const Homepage = () => {
   <option value="nameasc">By name asc</option>
   <option value="namedesc">By name desc</option>
 </select>
- </div>
+      </div>
+      <div className="search">
+      
+        <input type="text" id = "searchicon" name = "searchicon" placeholder="Search notes" />
+          <label class="search-icon" for = "searchicon"><SearchIcon/></label>
+      </div>
   
       <Link to="/addnote">
         <div className="homepage-title">
